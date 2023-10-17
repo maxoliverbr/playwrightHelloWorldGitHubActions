1d9590fa6fc1840ea0d9e4130850343aa7df0543
maxoliverbr <max.oliver@cintrax.com.br>
2023-10-17 00:31:52 -0300

    Add video recorder

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,6 +1,12 @@
-from playwright.sync_api import Page
+import pytest
+from playwright.sync_api import Playwright, sync_playwright, expect
 
 
-def test_search(page: Page) -> None:
-    page.goto("https://www.google.com")
+def test_login(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    context = browser.new_context(record_video_dir="videos/")
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/")
 
+    # Make sure to close, so that videos are saved.
+    context.close()
