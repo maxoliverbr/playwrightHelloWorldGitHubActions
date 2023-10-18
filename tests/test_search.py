280e8fe24c834ae59ae353f9802fe78a4bcf0c5c
maxoliverbr <max.oliver@cintrax.com.br>
2023-10-17 23:42:15 -0300

    Add more site requests.

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,7 +6,12 @@ def test_login(playwright: Playwright):
     browser = playwright.chromium.launch(headless=True, slow_mo=500)
     context = browser.new_context(record_video_dir="reports/videos/")
     page = context.new_page()
-    page.goto("https://www.saucedemo.com/")
+    page.goto("https://www.amazon.com.br/")
+    page.locator('id=twotabsearchtextbox').fill('Yoda')
+    page.locator('id=nav-search-submit-button').click()
+    page.screenshot(timeout=1000)
+    page.locator('id=twotabsearchtextbox').fill('Darth Vader')
+    page.locator('id=nav-search-submit-button').click()
 
     # Make sure to close, so that videos are saved.
     context.close()
